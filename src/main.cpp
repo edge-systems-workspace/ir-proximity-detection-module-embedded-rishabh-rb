6f1c08cb8b79fcc5d5150cebc5d48259cde27520
Rishabh-rb <rishabh.barnwal_cs.aiml24@gla.ac.in>
2026-02-20 09:30:56 +0530

    Add IR obstacle detection system for Uno R4 WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,54 @@
 /**
  * @file main.cpp
  * @brief Embedded Obstacle Detection System using IR Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author rishabh-rb
+ * @date 2026-02-20
  *
  * @details
  * Reads digital input from IR sensor
  * and detects obstacle presence.
  */
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
+// TODO 1:
+// Define IR sensor digital pin (Use pin 2)
+#define IR_PIN 2
 
- // TODO 2:
- // Create variable to store sensor state
+// TODO 2:
+// Create variable to store sensor state
+int sensorState = 0;
 
 void setup() {
 
     // TODO 3:
     // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
 
     // TODO 4:
     // Configure IR pin as INPUT
+    pinMode(IR_PIN, INPUT);
 
     // TODO 5:
     // Print system initialization message
+    Serial.println("IR Obstacle Detection System - Initialized");
+    delay(1000);
 }
 
 void loop() {
 
     // TODO 6:
     // Read digital value from IR sensor
+    sensorState = digitalRead(IR_PIN);
 
     // TODO 7:
     // If obstacle detected
-    //     Print "Obstacle Detected"
-    // Else
-    //     Print "No Obstacle"
+    if (sensorState == LOW) {   // Most IR modules give LOW when obstacle detected
+        Serial.println("Obstacle Detected");
+    }
+    else {
+        Serial.println("No Obstacle");
+    }
 
     // TODO 8:
-    // Add small delay (200–500ms)
+    // Add small delay (300ms)
+    delay(300);
 }
